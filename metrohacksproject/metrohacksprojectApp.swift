a73cc6bf76e8a4e055a47973296de501a11f0877
Neel Vinchhi <neelvinchhi2007@gmail.com>
2023-12-17 00:19:59 +0530

    Initial Commit

---
+++ metrohacksproject/metrohacksprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  metrohacksprojectApp.swift
+//  metrohacksproject
+//
+//  Created by Neel Vinchhi on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct metrohacksprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
